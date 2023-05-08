9c4c48f487468bbd8fa42e445d1199bdbe27995c
LordoftheEyes12 <114725735+LordoftheEyes12@users.noreply.github.com>
2023-05-08 08:39:15 +0000

    richtige CREATE TABLE in table.php hinzugefügt

--- _admin_chat/table.php
+++ _admin_chat/table.php
@@ -12,8 +12,10 @@ try {
     $sql = "CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL
-    )";
+        password VARCHAR(255) NOT NULL,
+        is_admin BOOLEAN DEFAULT 0
+    );
+    ";
     
     $pdo->exec($sql);
     echo "Table 'users' created successfully.";
