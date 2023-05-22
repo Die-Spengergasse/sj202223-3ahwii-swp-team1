90f5d24c653e33481c9683662f6e6c8eb4a730b2
Ben Daschner <114725735+LordoftheEyes12@users.noreply.github.com>
2023-05-22 15:04:31 +0000

    Wortfilter und Längenbegrenzung hinzugefügt

--- general/post.php
+++ general/post.php
@@ -3,7 +3,7 @@ session_start();
 
 // Schimpfwörter die Gefiltert werden sollen
 $bad_words = array(
-    "damn", 
+    "damn", "idiot",
     "hell", 
     "shit", 
     "fuck", 
@@ -33,26 +33,31 @@ if (isset($_SESSION['name'])) {
     // Holt die Nachricht aus der POST Funktion von index.php
     $text = $_POST['text'];
 
-    // Hier werden die Schimpfwörter
-    foreach ($bad_words as $word) {
-        if (strpos($text, $word) !== false) {
-            $stars = str_repeat('*', strlen($word));
-            $text = str_ireplace($word, $stars, $text);
+    // Prüft, ob die Nachricht die maximale Länge überschreitet
+    if (strlen($text) > 255) {
+        echo "<span class='error'>Die Nachricht darf maximal 255 Zeichen enthalten.</span>";
+    } else {
+        // Hier werden die Schimpfwörter
+        foreach ($bad_words as $word) {
+            if (strpos($text, $word) !== false) {
+                $stars = str_repeat('*', strlen($word));
+                $text = str_ireplace($word, $stars, $text);
+            }
         }
-    }
 
-    // Stellt sicher, dass keine leeren Nachrichten gesendet werden (leerzeichen werden ignoriert, sprich auch mit drei leerzeichen wird nichts gesendet)
-    if (!empty(trim($text))) {
-        // Öffnet die Chatlog Datei
-        $fp = fopen("log.html", 'a');
+        // Stellt sicher, dass keine leeren Nachrichten gesendet werden (Leerzeichen werden ignoriert, sprich auch mit drei Leerzeichen wird nichts gesendet)
+        if (!empty(trim($text))) {
+            // Öffnet die Chatlog Datei
+            $fp = fopen("log.html", 'a');
 
-        // Formatiert die Nachricht mit Zeitstempel, Benutzername und Inhalt
-        $formattedMessage = "<div class='msgln'>(" . date("g:i A") . ") <b>" . $_SESSION['name'] . "</b>: " . stripslashes(htmlspecialchars($text)) . "<br></div>";
+            // Formatiert die Nachricht mit Zeitstempel, Benutzername und Inhalt
+            $formattedMessage = "<div class='msgln'>(" . date("g:i A") . ") <b>" . $_SESSION['name'] . "</b>: " . stripslashes(htmlspecialchars($text)) . "<br></div>";
 
-        // Schreibt die Nachricht in die Logdatei
-        fwrite($fp, $formattedMessage);
+            // Schreibt die Nachricht in die Logdatei
+            fwrite($fp, $formattedMessage);
 
-        // Schließt die Logdatei
-        fclose($fp);
+            // Schließt die Logdatei
+            fclose($fp);
+        }
     }
 }
